cbfaa1934f38129789ca61d09e2ede442a55b2dc
Dawid Paluszkiewicz <dawid.paluszkiewicz88@gmail.com>
2017-10-25 23:06:39 +0200

    Dodałem warunki w przypadku wystąpienia 1 lub 0

--- src/PrimeNumberMain.java
+++ src/PrimeNumberMain.java
@@ -4,7 +4,8 @@ public class PrimeNumberMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Jeśli chcesz sprawdzić, czy liczba jest liczbą pierwszą wybierz 1");
-        System.out.println("Jeśli chcesz sprawdzić ile jest liczba pierwszych do liczby którą podałeś wybierz 2");
+        System.out.print("Jeśli chcesz sprawdzić liczby pierwsze występujące przed liczbą którą podałeś wybierz 2\n" +
+                "Podaj numer wybranej akcji: ");
         int numberOfChoice = scanner.nextInt();
         int number;
 
@@ -14,6 +15,8 @@ public class PrimeNumberMain {
                 number = scanner.nextInt();
                 if(isPrime(number)){
                     System.out.println("Liczba " + number + " jest liczbą pierwszą");
+                }else{
+                    System.out.println("Liczba " + number + " nie jest liczba pierwszą");
                 }
                 break;
             case(2):
@@ -26,13 +29,16 @@ public class PrimeNumberMain {
                         System.out.println("Liczba " + i + " jest liczbą pierwszą");
                     }
                 }
+                if(number == 1 || number == 0){
+                    System.out.println("Przed liczbą " + number + " nie występują liczby pierwsze");
+                }
                 break;
 
         }
 
     }
     private static boolean isPrime(int number) {
-        if (number == 1) {
+        if (number == 1 || number == 0) {
             return false;
         }
         for (int i = 2; i <= number / 2; i++) {
